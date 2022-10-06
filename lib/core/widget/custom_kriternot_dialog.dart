735fee7b1bed1fdadfc0078fb80158fa5d91c2a9
byyasar <byyasar@gmail.com>
2022-10-06 16:14:09 +0300

    değerlendirme kriterleri görünüm ayarları yapıldı

--- lib/core/widget/custom_kriternot_dialog.dart
+++ lib/core/widget/custom_kriternot_dialog.dart
@@ -19,7 +19,7 @@ class CustomKriterDialog extends StatefulWidget {
     this.transaction,
     // required this.onClickedDone,
     required this.ogrenciId,
-   // required this.parametreler,
+    // required this.parametreler,
     required this.kriterler,
     required this.index,
   }) : super(key: key);
@@ -43,17 +43,23 @@ class _CustomKriterDialogState extends BaseState<CustomKriterDialog> {
   @override
   void initState() {
     super.initState();
-    _aciklamaController.text = _getTransactionAciklama().isEmpty ? '' : _getTransactionAciklama();
+    _aciklamaController.text =
+        _getTransactionAciklama().isEmpty ? '' : _getTransactionAciklama();
     _kriter1Controller.text = widget.kriterler![0].toString();
     _kriter2Controller.text = widget.kriterler![1].toString();
     _kriter3Controller.text = widget.kriterler![2].toString();
     _kriter4Controller.text = widget.kriterler![3].toString();
     _kriter5Controller.text = widget.kriterler![4].toString();
-    _toplam = int.tryParse(_kriter1Controller.text.isEmpty ? '0' : _kriter1Controller.text)! +
-        int.tryParse(_kriter2Controller.text.isEmpty ? '0' : _kriter2Controller.text)! +
-        int.tryParse(_kriter3Controller.text.isEmpty ? '0' : _kriter3Controller.text)! +
-        int.tryParse(_kriter4Controller.text.isEmpty ? '0' : _kriter4Controller.text)! +
-        int.tryParse(_kriter5Controller.text.isEmpty ? '0' : _kriter5Controller.text)!;
+    _toplam = int.tryParse(
+            _kriter1Controller.text.isEmpty ? '0' : _kriter1Controller.text)! +
+        int.tryParse(
+            _kriter2Controller.text.isEmpty ? '0' : _kriter2Controller.text)! +
+        int.tryParse(
+            _kriter3Controller.text.isEmpty ? '0' : _kriter3Controller.text)! +
+        int.tryParse(
+            _kriter4Controller.text.isEmpty ? '0' : _kriter4Controller.text)! +
+        int.tryParse(
+            _kriter5Controller.text.isEmpty ? '0' : _kriter5Controller.text)!;
     ////_viewModel.setKriterler(widget.kriterler!);
   }
 
@@ -65,135 +71,154 @@ class _CustomKriterDialogState extends BaseState<CustomKriterDialog> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      titlePadding: EdgeInsets.fromLTRB(0, 10, 0, 0),
       title: const Center(child: Text('Değerlendirme Kriterleri')),
-      content: Form(
-        key: formKey,
-        child: SingleChildScrollView(
-          child: SizedBox(
-            width: dyanmicWidth(.95),
-            child: Column(
-              children: <Widget>[
-                const Divider(height: 10),
-                /* Observer(builder: (_) {
-                    _toplam = //_viewModel.toplam;
-                    return Text('TOPLAM =$_toplam');
-                  }), */
+      content: Column(
+        children: [
+          Row(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              myCustomMenuButton(context, () {
+                _buildAllOkButtononPressed;
+              }, const Text('Tam'), IconsConstans.fullokIcon,
+                  Colors.blueAccent),
+              const SizedBox(width: 10),
+              myCustomMenuButton(context, () {
+                _buildOkButtononPressed(false);
+              }, const Text('Gelmedi'), IconsConstans.gelmediIcon, Colors.red),
+            ],
+          ),
+          Form(
+            key: formKey,
+            child: Expanded(
+              child: SingleChildScrollView(
+                child: SizedBox(
+                  width: dyanmicWidth(.90),
+                  child: Column(
+                    children: <Widget>[
+                      const Divider(height: 10),
+                      /* Observer(builder: (_) {
+                          _toplam = //_viewModel.toplam;
+                          return Text('TOPLAM =$_toplam');
+                        }), */
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    myCustomMenuButton(context, () {
-                      _buildAllOkButtononPressed;
-                    }, const Text('Tam'), IconsConstans.fullokIcon, Colors.blueAccent),
-                    myCustomMenuButton(context, () {
-                      _buildOkButtononPressed(false);
-                    }, const Text('Gelmedi'), IconsConstans.gelmediIcon, Colors.red),
-                  ],
-                ),
+                      //"buildTemrin(context, transactionsTemrin),
+                      TextFormField(
+                        style: const TextStyle(fontSize: 24),
+                        decoration: const InputDecoration(
+                            labelText: '1-Bilgi -20P',
+                            focusColor: Colors.blue,
+                            labelStyle: TextStyle(fontSize: 18)),
+                        controller: _kriter1Controller,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          int puan = int.tryParse(value.isEmpty ? '0' : value)!;
+                          if (puan >= 0 && puan <= 20) {
+                            widget.kriterler![0] =
+                                int.tryParse(value.isEmpty ? '0' : value)!;
+                            //_viewModel.setKriterler(widget.kriterler!);
+                          } else {
+                            _kriter1Controller.text = '';
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        style: const TextStyle(fontSize: 24),
+                        decoration: const InputDecoration(
+                            labelText: '2-Çözümü anlama ve aktarma -30P',
+                            focusColor: Colors.blue,
+                            labelStyle: TextStyle(fontSize: 18)),
+                        controller: _kriter2Controller,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          int puan = int.tryParse(value.isEmpty ? '0' : value)!;
+                          if (puan >= 0 && puan <= 30) {
+                            widget.kriterler![1] =
+                                int.tryParse(value.isEmpty ? '0' : value)!;
+                            //_viewModel.setKriterler(widget.kriterler!);
+                          } else {
+                            _kriter2Controller.text = '';
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        style: const TextStyle(fontSize: 24),
+                        decoration: const InputDecoration(
+                            labelText: '3-Doğru şekilde çalıştırma -30P',
+                            focusColor: Colors.blue,
+                            labelStyle: TextStyle(fontSize: 18)),
+                        controller: _kriter3Controller,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          int puan = int.tryParse(value.isEmpty ? '0' : value)!;
+                          if (puan >= 0 && puan <= 30) {
+                            widget.kriterler![2] =
+                                int.tryParse(value.isEmpty ? '0' : value)!;
+                            //_viewModel.setKriterler(widget.kriterler!);
+                          } else {
+                            _kriter3Controller.text = '';
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        style: const TextStyle(fontSize: 24),
+                        decoration: const InputDecoration(
+                            labelText: '4-Tasarım -10P',
+                            focusColor: Colors.blue,
+                            labelStyle: TextStyle(fontSize: 18)),
+                        controller: _kriter4Controller,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          int puan = int.tryParse(value.isEmpty ? '0' : value)!;
+                          if (puan >= 0 && puan <= 10) {
+                            widget.kriterler![3] =
+                                int.tryParse(value.isEmpty ? '0' : value)!;
+                            //_viewModel.setKriterler(widget.kriterler!);
+                          } else {
+                            _kriter4Controller.text = '';
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        style: const TextStyle(fontSize: 24),
+                        decoration: const InputDecoration(
+                            labelText: '5-Süre -10P',
+                            focusColor: Colors.blue,
+                            labelStyle: TextStyle(fontSize: 18)),
+                        controller: _kriter5Controller,
+                        keyboardType: TextInputType.number,
+                        textAlign: TextAlign.center,
+                        onChanged: (value) {
+                          int puan = int.tryParse(value.isEmpty ? '0' : value)!;
+                          if (puan >= 0 && puan <= 10) {
+                            widget.kriterler![4] =
+                                int.tryParse(value.isEmpty ? '0' : value)!;
+                            //_viewModel.setKriterler(widget.kriterler!);
+                          } else {
+                            _kriter5Controller.text = '';
+                          }
+                        },
+                      ),
 
-                const Divider(height: 10),
-                //"buildTemrin(context, transactionsTemrin),
-                TextFormField(
-                  style: const TextStyle(fontSize: 24),
-                  decoration: const InputDecoration(
-                      labelText: '1-Bilgi -20P', focusColor: Colors.blue, labelStyle: TextStyle(fontSize: 18)),
-                  controller: _kriter1Controller,
-                  keyboardType: TextInputType.number,
-                  textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    int puan = int.tryParse(value.isEmpty ? '0' : value)!;
-                    if (puan >= 0 && puan <= 20) {
-                      widget.kriterler![0] = int.tryParse(value.isEmpty ? '0' : value)!;
-                      //_viewModel.setKriterler(widget.kriterler!);
-                    } else {
-                      _kriter1Controller.text = '';
-                    }
-                  },
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Açıklama', focusColor: Colors.blue),
+                        controller: _aciklamaController,
+                        onChanged: (value) {
+                          //_viewModel.setAciklama(value);
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-                TextFormField(
-                  style: const TextStyle(fontSize: 24),
-                  decoration: const InputDecoration(
-                      labelText: '2-Çözümü anlama ve aktarma -30P',
-                      focusColor: Colors.blue,
-                      labelStyle: TextStyle(fontSize: 18)),
-                  controller: _kriter2Controller,
-                  keyboardType: TextInputType.number,
-                  textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    int puan = int.tryParse(value.isEmpty ? '0' : value)!;
-                    if (puan >= 0 && puan <= 30) {
-                      widget.kriterler![1] = int.tryParse(value.isEmpty ? '0' : value)!;
-                      //_viewModel.setKriterler(widget.kriterler!);
-                    } else {
-                      _kriter2Controller.text = '';
-                    }
-                  },
-                ),
-                TextFormField(
-                  style: const TextStyle(fontSize: 24),
-                  decoration: const InputDecoration(
-                      labelText: '3-Doğru şekilde çalıştırma -30P',
-                      focusColor: Colors.blue,
-                      labelStyle: TextStyle(fontSize: 18)),
-                  controller: _kriter3Controller,
-                  keyboardType: TextInputType.number,
-                  textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    int puan = int.tryParse(value.isEmpty ? '0' : value)!;
-                    if (puan >= 0 && puan <= 30) {
-                      widget.kriterler![2] = int.tryParse(value.isEmpty ? '0' : value)!;
-                      //_viewModel.setKriterler(widget.kriterler!);
-                    } else {
-                      _kriter3Controller.text = '';
-                    }
-                  },
-                ),
-                TextFormField(
-                  style: const TextStyle(fontSize: 24),
-                  decoration: const InputDecoration(
-                      labelText: '4-Tasarım -10P', focusColor: Colors.blue, labelStyle: TextStyle(fontSize: 18)),
-                  controller: _kriter4Controller,
-                  keyboardType: TextInputType.number,
-                  textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    int puan = int.tryParse(value.isEmpty ? '0' : value)!;
-                    if (puan >= 0 && puan <= 10) {
-                      widget.kriterler![3] = int.tryParse(value.isEmpty ? '0' : value)!;
-                      //_viewModel.setKriterler(widget.kriterler!);
-                    } else {
-                      _kriter4Controller.text = '';
-                    }
-                  },
-                ),
-                TextFormField(
-                  style: const TextStyle(fontSize: 24),
-                  decoration: const InputDecoration(
-                      labelText: '5-Süre -10P', focusColor: Colors.blue, labelStyle: TextStyle(fontSize: 18)),
-                  controller: _kriter5Controller,
-                  keyboardType: TextInputType.number,
-                  textAlign: TextAlign.center,
-                  onChanged: (value) {
-                    int puan = int.tryParse(value.isEmpty ? '0' : value)!;
-                    if (puan >= 0 && puan <= 10) {
-                      widget.kriterler![4] = int.tryParse(value.isEmpty ? '0' : value)!;
-                      //_viewModel.setKriterler(widget.kriterler!);
-                    } else {
-                      _kriter5Controller.text = '';
-                    }
-                  },
-                ),
-
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Açıklama', focusColor: Colors.blue),
-                  controller: _aciklamaController,
-                  onChanged: (value) {
-                    //_viewModel.setAciklama(value);
-                  },
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
       actions: <Widget>[
         Row(
@@ -227,7 +252,7 @@ class _CustomKriterDialogState extends BaseState<CustomKriterDialog> {
   }
 
   void _buildOkButtononPressed(bool geldi) {
-   /*  String key =
+    /*  String key =
         "${widget.parametreler![0]}-${widget.parametreler![1]}-${widget.parametreler![2]}-${widget.ogrenciId!}";
     if (geldi) {
       _addTransactionTemrinnot(key, widget.index!, widget.parametreler![2], widget.ogrenciId!, //_viewModel.toplam,
@@ -247,13 +272,13 @@ class _CustomKriterDialogState extends BaseState<CustomKriterDialog> {
   }
 
   String _getTransactionAciklama() {
-  /*   String key =
+    /*   String key =
         "${widget.parametreler![0]}-${widget.parametreler![1]}-${widget.parametreler![2]}-${widget.ogrenciId!}";
     final _box = TemrinnotBoxes.getTransactions();
     return TemrinnotListesiHelper(ApplicationConstants.boxTemrinNot).getItem(key, _box) != null
         ? TemrinnotListesiHelper(ApplicationConstants.boxTemrinNot).getItem(key, _box)!.notlar
         : ""; */
-        return "";
+    return "";
   }
 
 /*   Future _addTransactionTemrinnot(
@@ -269,4 +294,4 @@ class _CustomKriterDialogState extends BaseState<CustomKriterDialog> {
     final box = TemrinnotBoxes.getTransactions();
     box.put(key, transaction);
   }*/
-} 
+}
--- lib/core/widget/custom_menu_button.dart
+++ lib/core/widget/custom_menu_button.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
-Widget myCustomMenuButton(
-    BuildContext context, VoidCallback? voidCallback, Widget btnText, Icon btnIcon, Color? btnPrimary) {
+Widget myCustomMenuButton(BuildContext context, VoidCallback? voidCallback,
+    Widget btnText, Icon btnIcon, Color? btnPrimary) {
   //final text = btnText.cast<Text>;
   btnPrimary ?? Colors.blue;
 
   return ElevatedButton(
-    style: ElevatedButton.styleFrom(backgroundColor: Colors.red, textStyle: const TextStyle(fontSize: 18)),
+    style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.red, textStyle: const TextStyle(fontSize: 16)),
     onPressed: voidCallback,
     child: Row(
       mainAxisSize: MainAxisSize.min,
-      children: [btnIcon, const SizedBox(width: 10), btnText],
+      children: [btnIcon, const SizedBox(width: 2), btnText],
     ),
   );
 }
